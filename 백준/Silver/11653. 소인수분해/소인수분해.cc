b8a09d90943ab897b6d45d44effc0edb30b819e0
jayyoung <40835055+jungjayyoung@users.noreply.github.com>
2022-05-28 20:19:41 +0900

    [Silver V] Title: 소인수분해, Time: 28 ms, Memory: 41084 KB -BaekjoonHub

--- 백준/Silver/11653. 소인수분해/소인수분해.cc
+++ 백준/Silver/11653. 소인수분해/소인수분해.cc
@@ -5,31 +5,11 @@ using namespace std;
 
 int not_prime[10000001] = {1, 1, 0};
 
-void solve(){
-
-
-    for (int i = 2; i <= 10000000; i++) {
-
-        if (!not_prime[i]) {
-
-            for (int j = 2 * i; j <= 10000000; j += i) {
-
-                not_prime[j] = 1;
-
-            }
-
-        }
-    }
-
-
-
-}
 int main(){
 
     ios_base::sync_with_stdio(false);
     cin.tie(0);
 
-    solve();
 
     int n;
     int div_num = 2;
@@ -37,12 +17,13 @@ int main(){
 
     while (n != 1) {
 
-        if (!not_prime[div_num] && n % div_num == 0) {
+        if (n % div_num == 0) {
             cout << div_num << "\n";
             n /= div_num;
         }else{
             div_num++;
         }
+
     }
 
 
