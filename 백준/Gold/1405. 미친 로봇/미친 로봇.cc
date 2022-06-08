db39dce083fe75f5fd5c0e8294a623efaeb58582
jayyoung <40835055+jungjayyoung@users.noreply.github.com>
2022-06-08 23:14:08 +0900

    [Gold V] Title: 미친 로봇, Time: 28 ms, Memory: 2724 KB -BaekjoonHub

---
+++ 백준/Gold/1405. 미친 로봇/미친 로봇.cc
@@ -0,0 +1,70 @@
+#include <iostream>
+
+
+using namespace std;
+
+int board[300][300]; // 150,150 에서 시작
+int visited[300][300];
+int s;
+int e;
+int n;
+int w;
+int num;
+
+int dx[4] = {1, -1, 0, 0};
+int dy[4] = {0, 0, -1, 1};
+double dpr[4];
+double ans = 0;
+int t= 0;
+
+void dfs(int x, int y,int cnt, double pr){
+
+    if (cnt >= num) {
+
+        ans += pr;
+        return;
+    }
+
+    for (int i = 0; i < 4; i++) {
+        //0: 서, 1: 북, 2: 동, 3: 남
+
+        int nextx = dx[i] + x;
+        int nexty = dy[i] + y;
+
+
+        if (!visited[nexty][nextx]) {
+            visited[nexty][nextx] = 1;
+            dfs(nextx, nexty, cnt + 1, pr * dpr[i]);
+            visited[nexty][nextx] = 0;
+
+        }
+    }
+
+}
+
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    cin >> num;
+    for (int i = 0; i < 4; i++) {
+        int a;
+        cin >> a;
+        dpr[i] = (double)a / 100;
+
+    }
+
+
+    fill(&visited[0][0], &visited[300][0], 0);
+
+    visited[150][150] = 1;
+    dfs(150, 150, 0, 1);
+
+    cout.precision(10);
+    cout << ans;
+
+
+    return 0;
+
+}
