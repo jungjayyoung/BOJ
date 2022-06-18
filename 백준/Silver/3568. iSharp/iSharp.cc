5eaf9b13c4edbead00c8893bf046d1ec03ae24cb
jayyoung <40835055+jungjayyoung@users.noreply.github.com>
2022-06-18 22:12:53 +0900

    [Silver III] Title: iSharp, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/3568. iSharp/iSharp.cc
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <deque>
+using namespace std;
+
+vector<string> v;
+
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    string str;
+    string start;
+
+    getline(cin, str);
+
+    string tmp;
+    for (int i = 0; i < str.size(); i++) {
+
+        if (str[i] != ' ' && str[i] != ',' && str[i] != ';') {
+            tmp += str[i];
+        }else{
+            if(!tmp.empty()){
+                v.push_back(tmp);
+            }
+
+            tmp = "";
+        }
+    }
+
+    for (int i = 1; i < v.size(); i++) {
+        string a;
+        string b;
+
+        for (int j = 0; j < v[i].size(); j++) {
+            if(isalpha(v[i][j])){
+                a += v[i][j];
+            } else {
+                b += v[i][j];
+            }
+
+        }
+        reverse(b.begin(), b.end());
+
+        for (int j = 0; j < b.size(); j++) {
+            if (b[j] == '[') {
+                b[j] = ']';
+            } else if (b[j] == ']') {
+                b[j] = '[';
+            }
+        }
+
+        cout << v[0] << b << " " << a << ';' << "\n";
+
+    }
+
+
+    return 0;
+}
