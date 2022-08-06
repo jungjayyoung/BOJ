534eb367f20219a762bd8d68923fa1c313bd6dc3
jayyoung <40835055+jungjayyoung@users.noreply.github.com>
2022-08-06 19:49:31 +0900

    [Gold II] Title: 후위 표기식, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/1918. 후위 표기식/후위 표기식.cc
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <stack>
+#include <string>
+
+
+
+using namespace std;
+
+stack<char> g;//괄호를 담을 스택
+stack<char> alp;//문자를 담을 스택
+stack<char> op;//연산자를 담을 스택
+
+
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    string s;
+    cin >> s;
+
+
+    for (int i = 0; i < s.size(); i++) {
+
+        if(s[i] >= 'A' && s[i] <= 'Z'){
+
+            cout << s[i];
+        }else if(s[i] == '+' || s[i] == '-' || s[i] == '*' || s[i] == '/' || s[i] == '('){
+
+            if(op.empty()){
+                op.push(s[i]);
+                continue;
+            }
+            if(s[i] == '-' || s[i] == '+'){
+                if (op.top() == '*' || op.top() == '/') {
+
+                    while (!op.empty() && op.top() != '(') {
+                        cout << op.top();
+                        op.pop();
+                    }
+                }else if(op.top() == '-' || op.top() == '+'){
+                    cout << op.top();
+                    op.pop();
+                }
+            }else if(s[i] == '*' || s[i] =='/'){
+
+                while (!op.empty() && (op.top() == '*' || op.top() == '/')) {
+                    cout << op.top();
+                    op.pop();
+                }
+
+            }
+            op.push(s[i]);
+        } else if (s[i] == ')') {
+
+            while(!op.empty() && op.top() != '('){
+                cout << op.top();
+                op.pop();
+            }
+            op.pop();
+        }
+
+    }
+
+    while(!op.empty()){
+        cout << op.top();
+        op.pop();
+    }
+
+
+
+
+
+    return 0;
+}
