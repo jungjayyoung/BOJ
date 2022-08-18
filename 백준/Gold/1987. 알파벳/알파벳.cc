715153ce658aa3338691e026ed3f803a9b723132
jayyoung <40835055+jungjayyoung@users.noreply.github.com>
2022-08-18 10:20:10 +0900

    [Gold IV] Title: 알파벳, Time: 468 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/1987. 알파벳/알파벳.cc
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <algorithm>
+
+
+using namespace std;
+
+char board[21][21];
+int visited[27];
+int board_visited[21][21];
+int dx[4] = {-1, 0, 1, 0};
+int dy[4] = {0, 1, 0, -1};
+int r, c;
+int ans = 1;
+
+void dfs(int xx, int yy,int cnt){
+
+    ans = max(cnt, ans);
+
+
+    for (int i = 0; i < 4; i++) {
+
+        int next_y = yy + dy[i];
+        int next_x = xx + dx[i];
+        if(next_x < 0 || next_y < 0 || next_x >= c || next_y >= r) continue;
+
+        int next_alp = board[next_y][next_x] - 'A';
+        if (!visited[next_alp] && !board_visited[next_y][next_x]) {
+            visited[next_alp] = 1;
+            board_visited[next_y][next_x] = 1;
+            dfs(next_x, next_y, cnt + 1);
+            visited[next_alp] = 0;
+            board_visited[next_y][next_x] = 0;
+        }
+
+
+    }
+
+
+
+}
+
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+
+    cin >> r >> c;
+
+
+    for (int y = 0; y < r; y++) {
+
+        for (int x = 0; x < c; x++) {
+
+            cin >> board[y][x];
+
+        }
+    }
+
+    board_visited[0][0] = 1;
+    visited[board[0][0] - 'A'] = 1;
+
+    dfs(0, 0, 1);
+
+
+    cout << ans;
+
+    return 0;
+}
