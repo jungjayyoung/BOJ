e69e3996bb68eb5c81fa973998fd9691b10fb8fd
jayyoung <40835055+jungjayyoung@users.noreply.github.com>
2022-11-13 02:54:45 +0900

    [Silver II] Title: 돌 게임 4, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/9658. 돌 게임 4/돌 게임 4.cc
+++ 백준/Silver/9658. 돌 게임 4/돌 게임 4.cc
@@ -11,17 +11,14 @@ int main(){
     int n;
     cin >> n;
     
-    dp[1] = 0;
-    dp[2] = 1; // sk가 이김
-    dp[3] = 0;
+    dp[2] = 1;
     dp[4] = 1;
   
     
-    for(int i = 5; i < 1001;i++){
-        if(dp[i - 1] && dp[i -3] && dp[i - 4]){
-            dp[i] = 0;
-         }else
+    for(int i = 5; i < 1001;++i){
+        if(!dp[i - 1] || !dp[i -3] || !dp[i - 4]){
             dp[i] = 1;
+         }
     }
     
     if(dp[n] == 1){
