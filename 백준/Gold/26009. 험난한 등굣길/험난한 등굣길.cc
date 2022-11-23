13161f6be656278fd6cbeb65118fc2c8e96d14a2
jayyoung <40835055+jungjayyoung@users.noreply.github.com>
2022-11-23 20:27:02 +0900

    [Gold III] Title: 험난한 등굣길, Time: 372 ms, Memory: 72516 KB -BaekjoonHub

--- 백준/Gold/26009. 험난한 등굣길/험난한 등굣길.cc
+++ 백준/Gold/26009. 험난한 등굣길/험난한 등굣길.cc
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <queue>
 #include <algorithm>
-#define NMAX 12345678
+
 using namespace std;
 
 int n, m, k;
@@ -11,8 +11,8 @@ int dy[] = {0, 1, 0, -1};
 int dx2[] = {-1, -1, 1, 1};
 int dy2[] = {-1, 1, -1, 1};
 
-int map[3001][3001];
-int dp[3001][3001];
+int map[3001][3001]; // n * m 판
+int visited[3001][3001];
 
 int main(){
     ios_base::sync_with_stdio(false);
@@ -25,37 +25,51 @@ int main(){
         int yy, xx, d;
         cin >> yy >> xx >> d;
 
-        for (int j = 0; j <= d; j++) {
-
-            for (int a = 0; a < 4; a++) {
-                int nx = xx + dx[a] * j;
-                int ny = yy + dy[a] * j;
-
-                if (nx < 1 || ny < 1 || nx > m || ny > n) continue;
-                map[ny][nx] = 1;
-            }
-
-
-            for (int a = 0; a < 4; a++) {
-                int nx = xx + dx2[a] * j;
-                int ny = yy + dy2[a] * j;
-
-                if (nx < 1 || ny < 1 || nx > m || ny > n) continue;
-                if ((abs(nx - xx) + abs(ny - yy)) <= d) {
-                    map[ny][nx] = 1;
-                }
-            }
-
-
+        if (d == 0) {
+            map[yy][xx] = 1;
         }
+
+        // 1. 위쪽부터 시작
+        int sx = xx;
+        int sy = yy - d;
+        if (sx >= 1 && sx <= m && sy >= 1 && sy <= n) map[sy][sx] = 1;
+
+        // 2. 남서쪽으로
+        while (sy < yy) {
+            if (sx >= 1 && sx <= m && sy >= 1 && sy <= n) map[sy][sx] = 1;
+            sx = sx - 1;
+            sy = sy + 1;
+        }
+
+
+        //3. 동남쪽으로
+        while (sx < xx) {
+            if (sx >= 1 && sx <= m && sy >= 1 && sy <= n) map[sy][sx] = 1;
+            sx = sx + 1;
+            sy = sy + 1;
+        }
+
+        //4. 북동쪽으로
+        while (sy > yy) {
+            if (sx >= 1 && sx <= m && sy >= 1 && sy <= n) map[sy][sx] = 1;
+            sx = sx + 1;
+            sy = sy - 1;
+        }
+        //5. 북서쪽으로
+
+        while(sx > xx){
+            if (sx >= 1 && sx <= m && sy >= 1 && sy <= n) map[sy][sx] = 1;
+            sx = sx - 1;
+            sy = sy - 1;
+        }
+
     }
 
 
     queue<pair<pair<int,int>,int>> q;
-    fill(&dp[0][0], &dp[3001][0], NMAX);
 
-    dp[1][1] = 0;
-    q.push({{1, 1}, 0});
+    visited[1][1] = 1;
+    q.push({{1, 1}, 0}); // x좌표, y좌표, 이동횟수
 
 
     int ans;
@@ -67,14 +81,22 @@ int main(){
 
         q.pop();
 
+        if (cury == n && curx == m) {
+            ans = cnt;
+            check = true;
+            break;
+        }
+
+
         for (int i = 0; i < 4; i++) {
             int nx = curx + dx[i];
             int ny = cury + dy[i];
 
             if (nx < 1 || ny < 1 || nx > m || ny > n || map[ny][nx] == 1) continue;
 
-            if (dp[ny][nx] > dp[cury][curx] + 1 && map[ny][nx] == 0) {
-                dp[ny][nx] = dp[cury][curx] + 1;
+            // 교통 정체가 아닌 길이면서 방문하지 않은 곳이면 방문처리해주고 큐에 넣는다.
+            if (visited[ny][nx] == 0 && map[ny][nx] == 0) {
+                visited[ny][nx] = 1;
                 q.push({{nx, ny}, cnt + 1});
             }
 
@@ -83,12 +105,18 @@ int main(){
 
 
     }
+//    for (int y = 1; y <= n; y++) {
+//
+//        for (int x = 1; x <= m; x++) {
+//            cout << map[y][x] << " ";
+//
+//        }
+//        cout << "\n";
+//    }
 
-
-
-    if (dp[n][m] != NMAX) {
+    if (check) {
         cout << "YES" << "\n";
-        cout << dp[n][m];
+        cout << ans;
     }else{
         cout << "NO";
     }
