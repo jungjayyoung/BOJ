4b92823ff8d8cf84c6c3128b6ce6215941673255
jayyoung <40835055+jungjayyoung@users.noreply.github.com>
2022-12-08 02:20:26 +0900

    [Silver II] Title: Treasure Cave, Time: 0 ms, Memory: 2288 KB -BaekjoonHub

---
+++ 백준/Silver/6125. Treasure Cave/Treasure Cave.cc
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int p, ns, t;
+
+vector<int> v[5001];
+vector<int> path;
+int visited[5001];
+bool check = false;
+int ans = 0;
+
+void dfs(int cur,int num){
+
+    if (cur == t) {
+
+        check = true;
+        ans = num;
+        return;
+    }
+
+
+    for (int i = 0; i < v[cur].size(); i++) {
+        int next = v[cur][i];
+
+        if (!visited[next]) {
+            visited[next] = 1;
+            path.push_back(next);
+            dfs(next,num + 1);
+            if(check) return;
+            visited[next] = 0;
+            path.pop_back();
+        }
+
+    }
+
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+
+    cin >> p >> ns >> t;
+
+    for (int i = 0; i < ns; i++) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        v[a].push_back(b);
+        v[a].push_back(c);
+    }
+
+    path.push_back(1);
+    visited[1] = 1;
+    dfs(1, 1);
+
+    cout << ans << "\n";
+
+    for (int i = 0; i < path.size(); i++) {
+        cout << path[i] << "\n";
+    }
+
+}
