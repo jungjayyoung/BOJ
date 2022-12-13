4f75ad095001298c2c92cca96cdbdd76d69b8999
jayyoung <40835055+jungjayyoung@users.noreply.github.com>
2022-12-14 01:46:30 +0900

    [Gold V] Title: 수 고르기, Time: 20 ms, Memory: 3684 KB -BaekjoonHub

---
+++ 백준/Gold/2230. 수 고르기/수 고르기.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+typedef long long ll;
+
+vector<ll> v;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n, m;
+
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++) {
+        ll num;
+        cin >> num;
+        v.push_back(num);
+    }
+
+    sort(v.begin(), v.end());
+
+    int i = 0;
+    int j = 0;
+    ll ans = 9876543211;
+
+    while (j < n && i < n) {
+        ll res = abs(v[i] - v[j]);
+        if (res >= m) {
+
+            if (ans > res) {
+                ans = res;
+            }
+
+            i++;
+            if (i >= j)
+                j++;
+
+        } else {
+            j++;
+        }
+    }
+    cout << ans;
+
+    return 0;
+}
