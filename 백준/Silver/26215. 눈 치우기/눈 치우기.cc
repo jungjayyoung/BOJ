6d2b7fb7ca2bfae48ce1ec9c48a4676e81da64e9
jayyoung <40835055+jungjayyoung@users.noreply.github.com>
2022-12-20 00:06:22 +0900

    [Silver III] Title: 눈 치우기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/26215. 눈 치우기/눈 치우기.cc
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+int n;
+int visited[101];
+
+vector<int> v;
+
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        int num;
+        cin >> num;
+        v.push_back(num);
+    }
+    if (n == 1) {
+        if (v[0] > 1440) {
+            cout << -1;
+        } else {
+            cout << v[0];
+        }
+        return 0;
+    }
+
+    sort(v.begin(), v.end());
+
+    int cnt = 0;
+
+    while (v[n - 1] > 0) {
+
+        cnt++;
+        if (n - 1 >= 0 && v[n - 1] > 0) {
+            v[n - 1]--;
+        }
+
+        if (n - 2 >= 0 && v[n - 2] > 0) {
+            v[n - 2]--;
+        }
+
+        sort(v.begin(), v.end());
+
+    }
+
+    if (cnt > 1440) {
+        cout << -1;
+    }else
+        cout << cnt;
+
+    return 0;
+}
