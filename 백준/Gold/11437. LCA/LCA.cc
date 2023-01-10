8e02a2147b3a28ee110cc695ba565f3a11bc5541
jayyoung <40835055+jungjayyoung@users.noreply.github.com>
2023-01-10 21:53:19 +0900

    [Gold III] Title: LCA, Time: 1104 ms, Memory: 7580 KB -BaekjoonHub

---
+++ 백준/Gold/11437. LCA/LCA.cc
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+
+using namespace std;
+
+
+int n, m;
+
+vector<int> v[50001];
+int parent[50001];
+int tree_level[50001];
+int visited[50001];
+
+
+void dfs(int cur,int pre){
+
+    visited[cur] = 1;
+    parent[cur] = pre;
+
+    for (int i = 0; i < v[cur].size(); i++) {
+        int next = v[cur][i];
+
+        if (!visited[next]) {
+            dfs(next, cur);
+        }
+
+    }
+
+
+}
+
+int find_parent(int u){
+
+    if (visited[u]) {
+
+        return u;
+    }
+
+    visited[u] = 1;
+
+    if (parent[u] == u) {
+        return u;
+    }
+
+    return find_parent(parent[u]);
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        parent[i] = i;
+    }
+
+    for (int i = 0; i < n - 1; i++) {
+        int a, b;
+        cin >> a >> b;
+        v[a].push_back(b);
+        v[b].push_back(a);
+    }
+
+
+    dfs(1, 1);
+    cin >> m;
+
+    for (int i = 0; i < m; i++) {
+        int a, b;
+        cin >> a >> b;
+        fill(visited, visited + n + 1, 0);
+
+
+        find_parent(a);
+
+
+        cout << find_parent(b) << "\n";
+
+
+
+    }
+
+
+
+
+    return 0;
+}
