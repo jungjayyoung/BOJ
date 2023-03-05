623200efbc1108699a11d440169bb6ce9774cefb
jayyoung <40835055+jungjayyoung@users.noreply.github.com>
2023-03-06 01:36:48 +0900

    [Silver III] Title: 전쟁 - 땅따먹기, Time: 6944 ms, Memory: 5192 KB -BaekjoonHub

---
+++ 백준/Silver/1270. 전쟁 － 땅따먹기/전쟁 － 땅따먹기.cc
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
+
+typedef long long ll;
+
+int n;
+
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    cin >> n;
+
+
+    for (int i = 0; i < n; i++) {
+
+        int num;
+
+        cin >> num;
+
+        map<ll,ll> m;
+
+
+        for (int j = 0; j < num; j++) {
+
+            ll idx;
+            cin >> idx;
+
+            if (m.find(idx) == m.end()) {
+                m.insert({idx, 1});
+            }else{
+                m[idx]++;
+            }
+
+        }
+
+        bool check = false;
+        for (auto j: m) {
+            //cout << i.first << " " << i.second << "\n";
+            //cout << "퍼센트: "<<(double)i.second / num * 100 << "\n";
+            double res = (double)j.second / num * 100;
+            if (res > 50) {
+                cout << j.first << "\n";
+                check = true;
+                break;
+            }
+
+        }
+
+        if (!check) {
+            cout << "SYJKGW" << "\n";
+        }
+
+        //cout << "\n\n";
+
+    }
+
+    return 0;
+}
